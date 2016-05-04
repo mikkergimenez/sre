29cccce5f8dbd386602ac17b38da654b66590774
Mikker Gimenez-Peterson <mikkergimenez@gmail.com>
2016-05-03 18:23:37 -0700

    Adding travis.yaml

--- Gemfile
+++ Gemfile
@@ -8,6 +8,12 @@ gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 # Windows does not come with time zone data
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
+group :development do
+  gem 'rake', '~> 10.4'
+  gem 'rspec', '~> 3.4'
+  gem 'capybara', '~> 2.5'
+end
+
 # Middleman Gems
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
---
+++ spec/features/index_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'index', :type => :feature do
+  before do
+    visit '/'
+  end
+
+  it 'has the correct title header' do
+    expect(page).to have_selector 'h1'
+    within 'h1' do
+      expect(page).to have_content /Middleman is Watching/i
+    end
+  end
+
+  it 'has a paragraph in there too' do
+    expect(page).to have_selector 'p.doc'
+  end
+end
